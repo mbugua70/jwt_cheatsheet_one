1551ecbf732c4c6686bf4a6ea7ef102f3a6c1775
john mbugua <johnymbugua1997@gmail.com>
2024-03-03 21:05:26 +0300

    jwt crash course project

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://myAtlasDBUser:123455@myatlasclusteredu.kmyoknr.mongodb.net/ninja_jwt_one?retryWrites=true&w=majority&appName=myAtlasClusterEDU";
+
+// port
+
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) =>
+    app.listen(port, () => {
+      console.log(`Listening to port: ${port}`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
+
+// cookies cheat sheet
+
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("set-cookie", "newUser=true");
+
+  //NOTE:: wen a cookie already exist a cookie will be updated into a new value instead.
+  res.cookie("newUser", false);
+
+  // NOTE: The second arguement is how long the cookie should last.
+  // we can also have third arguement of secure: true which will only send the cookie only on secure connection e.g https
+  // another arguement could be httpOnly which won't let us access the cookie from the front end using javascript.
+
+  res.cookie("isEmployee", true, { maxAge: 1000 * 60 * 60 * 24 });
+  res.send("You got a new cookie");
+});
+
+app.get("/read-cookies", (req, res) => {});
---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { isEmail } = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 characters"],
+  },
+});
+
+// mongoose hooks/ fires a function after and event or something has been saved to the database
+
+// userSchema.post("save", (doc, next) => {
+//   console.log("New user has been created successfully", doc);
+//   next();
+// });
+
+// function that fires befores saving to the database
+
+userSchema.pre("save", async function (next) {
+  // console.log("User about to be created", this);
+
+  // generate salt
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
